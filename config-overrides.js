9f5fcf842c06ff92c424fbd5d65c8a1d5b614d09
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-01-12 23:47:21 +0100

    Fallada la compresion de las imagenes.

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,41 @@
 const { override, addWebpackPlugin } = require("customize-cra");
-const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = override(
   addWebpackPlugin(
-    new ImageminPlugin({
-      pngquant: {
-        quality: "65-90",
-        speed: 4,
-      },
-      svgo: {
-        plugins: [{ removeViewBox: false }],
-      },
-      gifsicle: {
-        optimizationLevel: 2,
-      },
-      jpegtran: {
-        progressive: true,
-      },
+    new ImageMinimizerPlugin({
+      minimizer: [
+        {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Configuración de compresión aquí
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["mozjpeg", { progressive: true, quality: 65 }],
+              ["optipng", { optimizationLevel: 5 }],
+              [
+                "svgo",
+                {
+                  plugins: [
+                    {
+                      name: "removeViewBox",
+                      params: {
+                        active: false,
+                      },
+                    },
+                    {
+                      name: "addAttributesToSVGElement",
+                      params: {
+                        attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      ],
     })
   )
 );
